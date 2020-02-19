06cb378a590b8f0f32dfbcc5770207d286f5f26d
ilya trush <diesejl@gmail.com>
2020-02-19 16:00:06 +0300

    Добавление админского сообщения

--- bot.py
+++ bot.py
@@ -2,5 +2,4 @@ from aiogram import executor
 from handlers.commands import *
 
 if __name__ == '__main__':
-    executor.start_polling(dispatcher, skip_updates=True, )
-# on_startup=send_to_admin
+    executor.start_polling(dispatcher, skip_updates=True, on_startup=send_to_admin)

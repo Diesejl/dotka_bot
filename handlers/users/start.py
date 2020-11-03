671a133574ee883a005f2a08d399e3565662d7da
ilya trush <diesejl@gmail.com>
2020-11-03 17:17:25 +0300

    Добавлен костыль для удаления реплайкиборд.

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,4 +1,4 @@
-from aiogram.types import Message
+from aiogram.types import Message, ReplyKeyboardRemove
 from aiogram.dispatcher.filters.builtin import CommandStart
 
 from keyboards.default import menu
@@ -10,3 +10,9 @@ async def bot_start(message: Message):
     await message.answer(f"Привет, {message.from_user.first_name} бот еще в разработке,"
                          f"так что часто бывает недоступен. Доступные кнопки располагаются чуть ниже",
                          reply_markup=menu)
+
+
+@dp.message_handler(commands="kekus")
+async def kill_menu(message: Message):
+    remove_kb = ReplyKeyboardRemove()
+    await message.answer(text="ok...", reply_markup=remove_kb)
